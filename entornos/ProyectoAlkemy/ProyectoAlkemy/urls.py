01a7752102948e60fafa2926830a26081368ea5c
Mauricio Mamani <maurobvx@gmail.com>
2023-05-19 00:42:04 -0300

    Registro y activacion de empleado agregado

--- entornos/ProyectoAlkemy/ProyectoAlkemy/urls.py
+++ entornos/ProyectoAlkemy/ProyectoAlkemy/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include("reservas.urls")),
+    path('reservas/', include("reservas.urls")),
 ]
--- entornos/ProyectoAlkemy/reservas/urls.py
+++ entornos/ProyectoAlkemy/reservas/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= "index"),
-    
+    path('empleados/crear/',views.registrar_empleado,name="registrar_empleado"),
+    path('empleados/activar/<int:id_empleado>',views.activar_empleado, name="activar_empleado")
 ]
