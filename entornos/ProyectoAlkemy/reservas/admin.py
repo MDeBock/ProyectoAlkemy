00b5f780cf3cc75da09275bc09d1df7ad722ab67
MDeBock <102190577+MDeBock@users.noreply.github.com>
2023-05-24 19:30:44 -0300

    las tareas de jose

--- entornos/ProyectoAlkemy/reservas/admin.py
+++ entornos/ProyectoAlkemy/reservas/admin.py
@@ -14,3 +14,16 @@ class ServicioAdmin(admin.ModelAdmin):
     list_display = ("id", "nombre", "descripcion", "activo")
     search_fields = ("nombre")
     list_filter = ("activo" )
+    
+class EmpleadoAdmin(admin.ModelAdmin):
+    empleado = Empleado
+    list_display = ("id", "nombre", "apellido", "numero_legajo", "activo")
+    search_fields = ("nombre", "apellido")
+    list_filter = ("activo" )
+    
+class CoordinadorAdmin(admin.ModelAdmin):
+    coordinaro = Coordinador
+    list_display = ("id", "nombre", "apellido", "dni", "fecha_alta", "activo")
+    search_fields = ("nombre", "apellido")
+    list_filter = ("activo" )
+    
